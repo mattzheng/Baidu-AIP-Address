ee1e6b4b513316713d359080b9efd862ee571a0e
Matt Zheng <mattzheng@yeah.net>
2020-10-09 11:44:58 +0800

    Update baidu_address_detection_v1.py

--- baidu_address_detection_v1.py
+++ baidu_address_detection_v1.py
@@ -30,8 +30,6 @@ class address_detection:
     def address(self,text):
         access_token = self.get_access_token()
         post_url = 'https://aip.baidubce.com/rpc/2.0/nlp/v1/address?charset=UTF-8&access_token={}'.format(access_token)
-        #https://aip.baidubce.com/rpc/2.0/nlp/v1/lexer?charset=UTF-8&access_token=24.f9ba9c5241b67688bb4adbed8bc91dec.2592000.1485570332.282335-8574074
-        
         headers = {'Content-Type': 'application/json'}
         data = {"text":text}
         r = post(post_url, json.dumps(data), headers=headers)
